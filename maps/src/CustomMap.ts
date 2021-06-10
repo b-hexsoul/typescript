9b7dcee02683c4f58e685e842b3121b7920a5ace
Brandon Hexsel <66162988+b-hexsoul@users.noreply.github.com>
2021-06-10 02:30:22 -0700

    Class implements Interface

--- maps/src/CustomMap.ts
+++ maps/src/CustomMap.ts
@@ -1,8 +1,9 @@
-interface Mappable {
+export interface Mappable {
   location: {
     lat: number;
     lng: number;
   };
+  markerContent(): string;
 }
 
 export class CustomMap {
@@ -16,12 +17,17 @@ export class CustomMap {
   }
 
   addMarker(mappable: Mappable): void {
-    new google.maps.Marker({
+    const marker = new google.maps.Marker({
       map: this.googleMap,
       position: {
         lat: mappable.location.lat,
         lng: mappable.location.lng,
       },
+    });
+    const content = mappable.markerContent();
+    marker.addListener('click', () => {
+      const infoWindow = new google.maps.InfoWindow({ content });
+      infoWindow.open(this.googleMap, marker);
     });
   }
 }
