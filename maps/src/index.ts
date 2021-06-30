67e95191f2d4897522fd410995d331ecb0cd8a21
Brandon Hexsel <66162988+b-hexsoul@users.noreply.github.com>
2021-06-30 04:30:44 -0700

    'init new project'

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -2,10 +2,9 @@ import { User } from './User';
 import { Company } from './Company';
 import { CustomMap } from './CustomMap';
 
-const user = new User();
-const company = new Company();
 const customMap = new CustomMap('map');
-customMap.addMarker(user);
-customMap.addMarker(company);
-console.log(user);
-console.log(company);
+for (let i = 0; i < 20; i++) {
+  let comp = new Company()
+  customMap.addMarker(comp)
+}
+
