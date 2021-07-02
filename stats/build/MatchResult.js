9f53a87fecd1f78199ba2622cd1d424860302a0c
Brandon Hexsel <66162988+b-hexsoul@users.noreply.github.com>
2021-07-02 04:40:13 -0700

    'Move enum to file'

---
+++ stats/build/MatchResult.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.MatchResult = void 0;
+// enum - enumeration - this is simply a signal to other engineers
+var MatchResult;
+(function (MatchResult) {
+    MatchResult["HomeWin"] = "H";
+    MatchResult["AwayWin"] = "A";
+    MatchResult["Draw"] = "D";
+})(MatchResult = exports.MatchResult || (exports.MatchResult = {}));
---
+++ stats/src/MatchResult.ts
@@ -0,0 +1,6 @@
+// enum - enumeration - this is simply a signal to other engineers
+export enum MatchResult {
+  HomeWin = 'H',
+  AwayWin = 'A',
+  Draw = 'D',
+}
