990247e5252a25e5e6e47424122b8f612d7f1d06
Brandon Hexsel <66162988+b-hexsoul@users.noreply.github.com>
2021-07-02 04:39:58 -0700

    'Add parseDate util'

---
+++ stats/src/utils.ts
@@ -0,0 +1,9 @@
+// caution: requires specific datastring input
+
+export const dateStringToDate = (dateString: string): Date => {
+  const dateParts = dateString.split('/').map((value: string): number => {
+    return parseInt(value);
+  });
+
+  return new Date(dateParts[2], dateParts[1] - 1, dateParts[0]);
+};
